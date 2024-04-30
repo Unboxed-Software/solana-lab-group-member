feb7e0f42eb0cb59141a7d56173d01d58e8afcdb
PBillingsby <philbillingsby@gmail.com>
2024-04-30 10:23:02 -0400

    change to local validator

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import { initializeKeypair, makeKeypairs } from "@solana-developers/helpers"
-import { Cluster, Connection, clusterApiUrl } from "@solana/web3.js"
+import { Connection } from "@solana/web3.js"
 import dotenv from "dotenv"
 // import { createGroup } from "./create-group" // We'll uncomment this later
 // import { uploadOffChainMetadata } from "./helpers" // We'll uncomment this later
 import { TokenMetadata } from "@solana/spl-token-metadata"
 dotenv.config()
 
-const CLUSTER: Cluster = "devnet"
-
-/**
- * Create a connection and initialize a keypair if one doesn't already exists.
- * If a keypair exists, airdrop a sol if needed.
- */
-const connection = new Connection(clusterApiUrl(CLUSTER))
+const connection = new Connection("http://127.0.0.1:8899")
 
 const payer = await initializeKeypair(connection)
 
