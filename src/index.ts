658d567f1cc3565e104b855fa3ad0f5ac20a34d1
Coach Chuck <christian@jito.wtf>
2024-06-08 12:16:10 -0600

    almost there

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 // import { createGroup } from "./create-group" // We'll uncomment this later
 // import { uploadOffChainMetadata } from "./helpers" // We'll uncomment this later
 import { TokenMetadata } from "@solana/spl-token-metadata"
+import { LabNFTMetadata, uploadOffChainMetadata } from "./helpers"
 dotenv.config()
 
 const connection = new Connection("http://127.0.0.1:8899")
@@ -16,10 +17,34 @@ const maxMembers = 3
 const [collectionMintKeypair] = makeKeypairs(1)
 
 // (Group Lesson) CREATE COLLECTION METADATA
+const collectionMetadata: LabNFTMetadata = {
+	mint: collectionMintKeypair,
+	imagePath: "assets/collection.png",
+	tokenName: "cool-cats-collection",
+	tokenDescription: "Collection of Cool Cat NFTs",
+	tokenSymbol: "MEOW",
+	tokenExternalUrl: "https://solana.com/",
+	tokenAdditionalMetadata: {},
+	tokenUri: "",
+}
 
 // (Group Lesson) UPLOAD OFF-CHAIN METADATA
+collectionMetadata.tokenUri = await uploadOffChainMetadata(
+	payer,
+	collectionMetadata
+)
 
 // (Group Lesson) FORMAT TOKEN METADATA
+const collectionTokenMetadata: TokenMetadata = {
+	name: collectionMetadata.tokenName,
+	mint: collectionMintKeypair.publicKey,
+	symbol: collectionMetadata.tokenSymbol,
+	uri: collectionMetadata.tokenUri,
+	updateAuthority: payer.publicKey,
+	additionalMetadata: Object.entries(
+		collectionMetadata.tokenAdditionalMetadata || []
+	).map(([trait_type, value]) => [trait_type, value]),
+}
 
 // (Group Lesson) CREATE GROUP
 
