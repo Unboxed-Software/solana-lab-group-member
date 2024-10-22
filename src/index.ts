f132fbb4161d4246b1ee14fd5a88cd1a7d70ab2f
Mike MacCana <mike.maccana@gmail.com>
2024-10-22 11:58:48 -0400

    Format, no changes.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
 // import { createTokenGroup } from "./create-group"// We'll uncomment this later
 // import { createTokenMember } from "./create-member" // We'll uncomment this later
-import { getExplorerLink, initializeKeypair, makeKeypairs } from "@solana-developers/helpers"
-import { Connection } from "@solana/web3.js"
-import dotenv from "dotenv"
-import { TokenMetadata } from "@solana/spl-token-metadata"
-import { LabNFTMetadata, uploadOffChainMetadata } from "./helpers"
-import { getGroupMemberPointerState, getGroupPointerState, getMetadataPointerState, getMint, getTokenGroupMemberState, getTokenMetadata, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token"
-dotenv.config()
+import {
+  getExplorerLink,
+  initializeKeypair,
+  makeKeypairs,
+} from "@solana-developers/helpers";
+import { Connection } from "@solana/web3.js";
+import dotenv from "dotenv";
+import { TokenMetadata } from "@solana/spl-token-metadata";
+import { LabNFTMetadata, uploadOffChainMetadata } from "./helpers";
+import {
+  getGroupMemberPointerState,
+  getGroupPointerState,
+  getMetadataPointerState,
+  getMint,
+  getTokenGroupMemberState,
+  getTokenMetadata,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+dotenv.config();
 
-const connection = new Connection("http://127.0.0.1:8899")
+const connection = new Connection("http://127.0.0.1:8899");
 
-const payer = await initializeKeypair(connection)
+const payer = await initializeKeypair(connection);
 
-const decimals = 0
-const maxMembers = 3
+const decimals = 0;
+const maxMembers = 3;
 
 const [groupMintKeypair, cat0Mint, cat1Mint, cat2Mint] = makeKeypairs(4);
 
